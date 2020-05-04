7ce35b7d53ca696515dfe4fad487d5fa44a5385f
Jordan Noe <jordanvinnoe@gmail.com>
2020-05-04 17:06:46 -0400

    prototype with hard-coded youtube

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).foundation();
+
+$(document).ready(function() {
+    //create functions to hold query params
+    //pass those function returns to the ajax call
+
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(//function to display on page);
+
+
+      
+})
